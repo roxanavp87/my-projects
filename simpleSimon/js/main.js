6067aeb6df2ecd47162f51bcd0527e0995f6af76
roxanavp87 <roxanavp87@gmail.com>
2017-05-02 11:24:14 -0500

    - fancy style ++

--- simpleSimon/js/main.js
+++ simpleSimon/js/main.js
@@ -7,9 +7,8 @@ $('document').ready(function () {
     var sequence = [];
     var random;
     var index = 0, counter = 0, round = 1;
-    // var gameOver = false;
-    // var displayingSequence = false;
-    // var displayingColor = false;
+    var gameOver = false;
+    var displayingColor = false;
     var on = false, off = true;
 
     var index_seq = 0;
@@ -33,17 +32,20 @@ $('document').ready(function () {
 
     function animateBoxes() {
         $('#' + sequence[index_seq]).animate({opacity: 0.2}, 600).animate({opacity: 1}, 400, function () {
-            // console.log('animating box ' + sequence[index_seq]);
             index_seq++;
             if(index_seq < sequence.length) {
                 animateBoxes();
+            } else {
+                displayingColor = false;
+                $('#text').html('Repeat the sequence you just saw!');
             }
         });
     }
 
     function addColorToSequence() {
         // add a new color to the sequence randomly
-        // displayingSequence = true;
+        $('#text').html('Watch the sequence!');
+        displayingColor = true;
         random = Math.round(Math.random()*3);
         numToColor(random, index);
         index++;
@@ -57,27 +59,40 @@ $('document').ready(function () {
         counter = 0;
         sequence = [];
         round = 1;
+        displayingColor = false;
+        on = false;
+        off = true;
     }
 
     $('#start').click(function () {
-        if(on && !off) {
+
+        if(on && !off && !gameOver) {
+            $('#start').prop('disabled', true);
             addColorToSequence();
             $('#round').val(round);
+        }
+        if(gameOver) {
+            $('#start').removeClass('btn-warning').addClass('btn-danger').html('START').css('left','13%');
+            $('#round').val(0).css('left', '57%');
+            reset();
+            $('#text').html('Click START to begin!');
+            gameOver = false;
+            on = true;
+            off = false;
         }
     });
 
     $('.shapes').click(function () {
 
-        if(on && !off) {
+        if(on && !off && !displayingColor) {
 
             $(this).animate({opacity: 0.2}, 600).animate({opacity: 1}, 400, function () {
-                console.log('animating box ' + $(this).attr('id'));
 
                 if (counter < index) {
                     if ($(this).attr('id') === sequence[counter]) {
                         counter++;
                         if (counter === index) {
-                            console.log('success');
+                            // player put the right sequence
                             round++;
                             $('#round').val(round);
                             addColorToSequence();
@@ -85,9 +100,11 @@ $('document').ready(function () {
                         }
 
                     } else {
-                        console.log('game over');
-                        $('#round').val('!!');
+                        $('#text').html('GAME OVER!');
+                        gameOver = true;
+                        $('#round').val('!!').css('left', '63%');
                         reset();
+                        $('#start').prop('disabled', false).removeClass('btn-danger').addClass('btn-warning').html('RESTART').css('left','8%');
                     }
                 }
             });
@@ -99,6 +116,8 @@ $('document').ready(function () {
     $('#on').click(function () {
         on = true;
         off = false;
+        $('#text').html('Click START to begin!');
+        $('#start').prop('disabled', false);
     });
 
     $('#off').click(function () {
@@ -106,6 +125,9 @@ $('document').ready(function () {
         off = true;
         reset();
         $('#round').val(0);
+        $('#text').html('Click ON to begin!');
     });
+
+
 
 });
